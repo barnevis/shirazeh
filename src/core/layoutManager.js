9ca7b459bcb7a6b3430be8b2e287e1ab91be10cf
alirho <alireza.hokmabadi@protonmail.com>
2025-11-08 12:39:49 +0330

    fixed menu icon with navbar plugin

--- src/core/layoutManager.js
+++ src/core/layoutManager.js
@@ -96,7 +96,7 @@ export class LayoutManager {
             const toggleButton = document.createElement('button');
             toggleButton.className = 'sidebar-toggle';
             toggleButton.setAttribute('aria-label', 'باز و بسته کردن منو');
-            toggleButton.innerHTML = '☰';
+            toggleButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>`;
             rootElement.appendChild(toggleButton);
         }
         
